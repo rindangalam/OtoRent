0f581d5cde507167640e8deb6f4f8768a1584067
Rindang Alam Nur Muhammad <rindangalam04@gmail.com>
2026-08-04 14:56:24 +0700

    refactor: remove dead code and unused components

--- resources/views/components/danger-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-status-danger text-on-error rounded-xl hover:brightness-110']) }}>
-    {{ $slot }}
-</button>
--- resources/views/components/secondary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-surface-container text-on-surface-variant rounded-xl border border-outline-variant/30 hover:bg-surface-container-high']) }}>
-    {{ $slot }}
-</button>
